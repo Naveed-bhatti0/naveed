728206b6dae0afbfe4dfb16c8df2df0e852c62b4
Naveed-bhatti0 <naveedalhassan08@gmail.com>
2026-01-14 16:18:39 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/naveed/"
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: "/naveed/", // 👈 IMPORTANT for GitHub Pages
+
+  server: {
+    host: "::",
+    port: 8080,
+  }
