edc32968f001ec1045e3c618354dd62dda93b657
Naveed-bhatti0 <naveedalhassan08@gmail.com>
2026-01-17 00:59:25 +0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: "/naveed/", // 👈 IMPORTANT for GitHub Pages
-
+export default defineConfig({
+  base: "/naveed/", // GitHub Pages ke liye important
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"), // ✅ Alias fix
+    },
+  },
   server: {
     host: "::",
     port: 8080,
   }
+});
