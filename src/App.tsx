e0d905fe5445a4a5f4236bad643e836f5abecb3b
Naveed-bhatti0 <naveedalhassan08@gmail.com>
2026-01-17 01:22:23 +0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+// ...
+
 const App = () => {
-  return <div style={{ color: "red" }}>App is working!</div>;
+  return (
+    <div>
+      <Header />
+      <HeroSection />
+      ...
+    </div>
+  );
 };
 
 export default App;
